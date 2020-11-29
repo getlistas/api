40cd7281fc8a1ab481121e238822de51437ba354
ndelvalle <nicolas.delvalle@gmail.com>
2020-11-29 18:40:36 -0300

    Create user migrations and sync-indexes script

---
+++ migrations/migrations/20201129-172729-00.create-users.js
@@ -0,0 +1,62 @@
+
+exports.name        = 'create-users';
+exports.description = 'Creates the base users';
+
+exports.isReversible = true;
+exports.isIgnored    = false;
+
+exports.up = async function (db, done) {
+  const now = new Date()
+
+  try {    
+    await db.collection('users').insertMany([
+      {
+        _id: new ObjectId('000000000000000000000000'),
+        email: 'nicolas.delvalle@gmail.com',
+        password: '$2b$12$Thcm/vRy4t1874eqCg0y1.Ote2H0TkJII39xnnsNBNcdbQ8KWK6q.', // Password1
+        slug: 'ndelvalle',
+        name: 'Nicolas Del Valle',
+        avatar: null,
+        verification_token: null,
+        created_at: now,
+        updated_at: now,
+        verified_at: now,
+      },
+      {
+        _id: new ObjectId('000000000000000000000001'),
+        email: 'gillchristiang@gmail.com',
+        password: '$2b$12$Thcm/vRy4t1874eqCg0y1.Ote2H0TkJII39xnnsNBNcdbQ8KWK6q.', // Password1
+        slug: 'gillchristian',
+        name: 'Christian Gill',
+        avatar: null,
+        verification_token: null,
+        created_at: now,
+        updated_at: now,
+        verified_at: now,
+      }
+    ]);
+
+    done()
+  } catch (err) {
+    done(err);
+  }
+};
+
+exports.down = async function (db, done) {
+  try {
+    await db.collection('users').deleteMany(
+      {
+        _id: {
+          $in: [
+            new ObjectId('000000000000000000000000'),
+            new ObjectId('000000000000000000000001'),
+          ]
+        },
+      }
+    );
+
+    done();
+  } catch (err) {
+    done(err);
+  }
+};
