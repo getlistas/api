518b5d4740623a6df5e1677f31d94ca5a5d05e4e
ndelvalle <nicolas.delvalle@gmail.com>
2020-12-07 21:48:02 -0300

    Implement update list tags

--- src/components/list/model.rs
+++ src/components/list/model.rs
@@ -19,16 +19,7 @@ pub struct List {
 impl List {
     pub fn new(body: ListCreate, user_id: ObjectId) -> Self {
         let now = chrono::Utc::now();
-        let tags = body
-            .tags
-            .map(|tags| {
-                tags.into_iter()
-                    .map(|tag| tag.to_lowercase().trim().to_owned())
-                    .filter(|tag| tag.len() >= 1)
-                    .unique()
-                    .collect()
-            })
-            .unwrap_or(vec![]);
+        let tags = body.tags.map(sanitize_tags).unwrap_or(vec![]);
 
         Self {
             id: None,
@@ -60,12 +51,25 @@ pub struct ListUpdate {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    pub tags: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
 }
 
 impl ListUpdate {
     pub fn new(update: &mut Self) -> &mut Self {
+        let tags = update.tags.clone().map(sanitize_tags).unwrap_or(vec![]);
+
         update.updated_at = Some(chrono::Utc::now());
+        update.tags = Some(tags);
         update
     }
+}
+
+fn sanitize_tags(tags: Vec<String>) -> Vec<String> {
+    tags.into_iter()
+        .map(|tag| tag.to_lowercase().trim().to_owned())
+        .filter(|tag| tag.len() >= 1)
+        .unique()
+        .collect()
 }
