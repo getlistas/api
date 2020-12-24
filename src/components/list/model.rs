96cc78d952a02ddcaca0802c9a383873fe9d49b5
ndelvalle <nicolas.delvalle@gmail.com>
2020-12-23 23:03:01 -0300

    Update bson datetime

--- src/components/list/model.rs
+++ src/components/list/model.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use wither::bson::DateTime;
 use serde::{Deserialize, Serialize};
 use wither::bson::{doc, oid::ObjectId};
 use wither::Model;
@@ -12,13 +13,13 @@ pub struct List {
     pub description: Option<String>,
     pub tags: Vec<String>,
 
-    pub created_at: chrono::DateTime<chrono::Utc>,
-    pub updated_at: chrono::DateTime<chrono::Utc>,
+    pub created_at: DateTime,
+    pub updated_at: DateTime,
 }
 
 impl List {
     pub fn new(body: ListCreate, user_id: ObjectId) -> Self {
-        let now = chrono::Utc::now();
+        let now = chrono::Utc::now().into();
         let tags = body.tags.map(sanitize_tags).unwrap_or(vec![]);
 
         Self {
@@ -53,14 +54,14 @@ pub struct ListUpdate {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub tags: Option<Vec<String>>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub updated_at: Option<DateTime>,
 }
 
 impl ListUpdate {
     pub fn new(update: &mut Self) -> &mut Self {
         let tags = update.tags.clone().map(sanitize_tags).unwrap_or(vec![]);
 
-        update.updated_at = Some(chrono::Utc::now());
+        update.updated_at = Some(chrono::Utc::now().into());
         update.tags = Some(tags);
         update
     }
