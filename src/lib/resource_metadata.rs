c67c96e72fe9b30c6e4835274488ff7cb4573836
ndelvalle <nicolas.delvalle@gmail.com>
2021-01-14 00:57:20 -0300

    Add title fallback to resource metadata

--- src/lib/resource_metadata.rs
+++ src/lib/resource_metadata.rs
@@ -16,33 +16,39 @@ pub async fn get_website_metadata(url: &Url) -> Result<WebsiteMetadata, reqwest:
     let res = reqwest::get(url.as_ref()).await?.text().await?;
 
     let document = Document::from(res.as_str());
+
+    let tag_title = document
+        .find(select::predicate::Name("title"))
+        .next()
+        .map(|node| node.text());
+
     let metas = document
         .find(select::predicate::Name("meta"))
         .filter(|node| node.attr("property").is_some())
         .collect::<Vec<select::node::Node>>();
 
-    let title: Option<String> = metas
+    let meta_title: Option<String> = metas
         .iter()
         .find(|meta| meta.attr("property").unwrap() == "og:title")
         .and_then(|meta| meta.attr("content"))
         .map(|value| value.to_owned());
 
-    let description: Option<String> = metas
+    let meta_description: Option<String> = metas
         .iter()
         .find(|meta| meta.attr("property").unwrap() == "og:description")
         .and_then(|meta| meta.attr("content"))
         .map(|value| value.to_owned());
 
-    let thumbnail: Option<String> = metas
+    let meta_thumbnail: Option<String> = metas
         .iter()
         .find(|meta| meta.attr("property").unwrap() == "og:image")
         .and_then(|meta| meta.attr("content"))
         .map(|value| value.to_owned());
 
     let metadata = WebsiteMetadata {
-        title,
-        description,
-        thumbnail,
+        title: meta_title.or(tag_title),
+        description: meta_description,
+        thumbnail: meta_thumbnail,
     };
 
     Ok(metadata)
