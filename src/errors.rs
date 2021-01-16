dc63539bf60967cbeacfc7120a4b467f07017af9
ndelvalle <nicolas.delvalle@gmail.com>
2021-01-15 23:17:54 -0300

    Format error as JSON

--- src/errors.rs
+++ src/errors.rs
@@ -1,14 +1,18 @@
 use actix_web::dev::HttpResponseBuilder;
-use actix_web::http;
+use actix_web::http::StatusCode;
 use actix_web::HttpResponse;
+use serde_json::json;
 use wither::bson;
 use wither::mongodb;
+use wither::mongodb::error::CommandError as MongoCommandError;
+use wither::mongodb::error::Error as MongoError;
+use wither::mongodb::error::ErrorKind as MongoErrorKind;
 use wither::WitherError;
 
 #[derive(thiserror::Error, Debug)]
 #[error("...")]
 pub enum ApiError {
-    #[error("Failed to read application shared Data")]
+    #[error("Failed to read application shared data")]
     ReadAppData(),
 
     #[error("Failed to parse request body")]
@@ -36,26 +40,46 @@ pub enum ApiError {
     HashPassword(#[from] actix_web::error::BlockingError<bcrypt::BcryptError>),
 }
 
+impl ApiError {
+    fn get_codes(&self) -> (StatusCode, u16) {
+        match *self {
+            // 4XX
+            ApiError::ParseRequestBody() => (StatusCode::BAD_REQUEST, 4041),
+            ApiError::ParseObjectID(_) => (StatusCode::BAD_REQUEST, 4042),
+            ApiError::WitherError(WitherError::Mongo(MongoError { ref kind, .. })) => {
+                let mongo_error = kind.as_ref();
+                match mongo_error {
+                    MongoErrorKind::CommandError(MongoCommandError { code: 11000, .. }) => {
+                        (StatusCode::BAD_REQUEST, 4043)
+                    }
+                    _ => (StatusCode::INTERNAL_SERVER_ERROR, 4044),
+                }
+            }
+
+            // 401
+            ApiError::JWT(_) => (StatusCode::UNAUTHORIZED, 4015),
+            ApiError::MissingAuthorizationToken {} => (StatusCode::UNAUTHORIZED, 4016),
+            ApiError::GoogleAuthentication {} => (StatusCode::UNAUTHORIZED, 4017),
+
+            // 5XX
+            ApiError::WitherError(_) => (StatusCode::INTERNAL_SERVER_ERROR, 5001),
+            ApiError::ReadAppData() => (StatusCode::INTERNAL_SERVER_ERROR, 5002),
+            ApiError::MongoError(_) => (StatusCode::INTERNAL_SERVER_ERROR, 5003),
+            ApiError::HashPassword(_) => (StatusCode::INTERNAL_SERVER_ERROR, 5004),
+        }
+    }
+}
+
 impl actix_web::error::ResponseError for ApiError {
     fn error_response(&self) -> HttpResponse {
-        HttpResponseBuilder::new(self.status_code())
-            .set_header(http::header::CONTENT_TYPE, "text/html; charset=utf-8")
-            .body(self.to_string())
-    }
+        let message = self.to_string();
+        let (status_code, code) = self.get_codes();
 
-    fn status_code(&self) -> http::StatusCode {
-        match *self {
-            ApiError::ReadAppData() => http::StatusCode::INTERNAL_SERVER_ERROR,
-            ApiError::WitherError(_) => http::StatusCode::INTERNAL_SERVER_ERROR,
-            ApiError::MongoError(_) => http::StatusCode::INTERNAL_SERVER_ERROR,
-            ApiError::HashPassword(_) => http::StatusCode::INTERNAL_SERVER_ERROR,
-
-            ApiError::ParseRequestBody() => http::StatusCode::BAD_REQUEST,
-            ApiError::ParseObjectID(_) => http::StatusCode::BAD_REQUEST,
-
-            ApiError::JWT(_) => http::StatusCode::UNAUTHORIZED,
-            ApiError::MissingAuthorizationToken {} => http::StatusCode::UNAUTHORIZED,
-            ApiError::GoogleAuthentication {} => http::StatusCode::UNAUTHORIZED,
+        if status_code == StatusCode::INTERNAL_SERVER_ERROR {
+            error!("Internal server error {}", &message);
         }
+
+        let body = json!({ "code": code, "message": message });
+        HttpResponseBuilder::new(status_code).json(body)
     }
 }
