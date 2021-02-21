1c419746cb2177aade78407ff715c8799ec89205
ndelvalle <nicolas.delvalle@gmail.com>
2021-02-21 00:56:43 -0300

    Create new architecture for models

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,18 +2,39 @@ pub mod integration;
 pub mod list;
 pub mod resource;
 pub mod user;
+use wither::bson::Document;
 
 use crate::database::Database;
+use crate::errors::Error;
 
 #[derive(Clone)]
 pub struct Models {
-  pub integration: integration::Integration,
+  db: Database,
 }
 
 impl Models {
-  pub fn new(database: Database) -> Self {
-    let integration = integration::Integration::new(database);
+  pub fn new(db: Database) -> Self {
+    Self { db }
+  }
 
-    Self { integration }
+  pub async fn create<T>(&self, mut model: T) -> Result<T, Error>
+  where
+    T: wither::Model + std::marker::Send,
+  {
+    model
+      .save(&self.db.conn, None)
+      .await
+      .map_err(Error::WitherError)?;
+
+    Ok(model)
+  }
+
+  pub async fn find_one<T>(&self, query: Document) -> Result<Option<T>, Error>
+  where
+    T: wither::Model + std::marker::Send,
+  {
+    T::find_one(&self.db.conn, query, None)
+      .await
+      .map_err(Error::WitherError)
   }
 }
