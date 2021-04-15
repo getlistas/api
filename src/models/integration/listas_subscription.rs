9494e57088b830079f3a71d4b78801f559c1abe0
Nicolas del Valle <nicolas.delvalle@gmail.com>
2021-04-15 02:05:00 -0300

    Setup subscription async actor handlers

--- src/models/integration/listas_subscription.rs
+++ src/models/integration/listas_subscription.rs
@@ -1,12 +1,16 @@
-use actix::{Actor, Context as ActorContext};
+use actix::{Actor, Context as ActorContext, Handler, Message};
+use actix::{ActorFuture, AsyncContext, ResponseActFuture};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use serde_json::Value as JSON;
-use wither::{bson::oid::ObjectId, ModelCursor};
+use wither::bson::doc;
+use wither::bson::oid::ObjectId;
 
 use crate::errors::Error;
 use crate::models::resource::Resource;
 use crate::models::Models;
+
+// pub type ResponseActFuture<A, I> = Pin<Box<dyn ActorFuture<Output = I, Actor = A>>>;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ListasSubscription {
@@ -43,4 +47,45 @@ impl Actor for ListasSubscriptionActor {
   fn stopped(&mut self, ctx: &mut ActorContext<Self>) {
     println!("Actor is stopped");
   }
+}
+
+impl Handler<SubscriptionPayload> for ListasSubscriptionActor {
+  type Result = ResponseActFuture<Self, Result<(), Error>>;
+
+  fn handle(&mut self, msg: SubscriptionPayload, ctx: &mut ActorContext<Self>) -> Self::Result {
+    let resource_id = msg.resource_id;
+    let models = self.models.clone();
+    let task = async move {
+      let resource = models
+        .find_one::<Resource>(doc! { "_id": &resource_id }, None)
+        .await?;
+
+      Ok::<(), Error>(())
+    };
+    let task = actix::fut::wrap_future::<_, Self>(task);
+    Box::pin(task)
+  }
+}
+
+#[derive(Message)]
+#[rtype(result = "Result<(), Error>")]
+pub struct SubscriptionPayload {
+  pub resource_id: ObjectId,
+}
+
+async fn send(models: &Models, resource_id: ObjectId) -> Result<(), Error> {
+  let resource = models
+    .find_one::<Resource>(doc! { "_id": &resource_id }, None)
+    .await
+    .unwrap();
+
+  let resource = match resource {
+    Some(resource) => resource,
+    None => {
+      error!("Resource not found, when handling subscription event");
+      return Ok(());
+    }
+  };
+
+  Ok(())
 }
